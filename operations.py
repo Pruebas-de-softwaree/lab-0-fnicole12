3a459ddc82a3cf4a0fc52b9aeb158d6a59540380
fnicole12 <fnicolegm12@outlook.com>
2025-08-22 02:54:31 +0000

    Hay un error en la funcion power, esperaba 4 de  (2, 2) y salio 0

--- operations.py
+++ operations.py
@@ -29,7 +29,26 @@ if __name__ == "__main__":
 
     print("start test")
 
+    #Suma
+    print(add(2,5))
+    print(add(-10,4))
+    print(add('a','b'))
 
-    print("henlo")
+    #Subract
+    print(subtract(10, 5))
+    print(subtract(-5, 5))
+
+    #Multiply
+    print(multiply(7, 2))
+    print(multiply(-10, 4))
+
+    #Divide
+    # print(divide(2, 0))
+    print(divide(10, 5))
+    print(divide(5, 3))
+
+    #Power
+    print(power(2, 2))
+
     print("end test")
 
